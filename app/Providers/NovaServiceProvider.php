eec4b71a08bb6846826bdea9bee0082ec1c88ae4
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-06-17 22:17:04 +0700

    First commit

---
+++ app/Providers/NovaServiceProvider.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Cards\Help;
+use Laravel\Nova\Nova;
+use Laravel\Nova\NovaApplicationServiceProvider;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+        Nova::createUserUsing(function ($command) {
+            return [
+                $command->ask('First Name'),
+                $command->ask('Last Name'),
+                $command->ask('Email Address'),
+                $command->secret('Password'),
+            ];
+        }, function ($first_name, $last_name, $email, $password) {
+            (new User)->forceFill([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'password' => Hash::make($password),
+                'email_verified_at' => now(),
+                'status' => 'accepted'
+            ])->save();
+        });
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+    }
+
+    /**
+     * Get the cards that should be displayed on the default Nova dashboard.
+     *
+     * @return array
+     */
+    protected function cards()
+    {
+        return [
+            new Help,
+        ];
+    }
+
+    /**
+     * Get the extra dashboards that should be displayed on the Nova dashboard.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
